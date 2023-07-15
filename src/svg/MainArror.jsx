82bdf96fe395654eb7446daf9b161698ad53ce6c
OnigiriKing <oooNikita87@gmail.com>
2023-07-15 18:58:41 +0700

    Fonts, animeted svg

--- src/svg/MainArror.jsx
+++ src/svg/MainArror.jsx
@@ -9,38 +9,42 @@ export default function ArrorSvg({width}) {
         className="arror-svg"
         xmlns="http://www.w3.org/2000/svg"
         width={width}
-        height="117"
-        viewBox="0 0 108 117"
-        fill="none"
+        height="200"
+        viewBox="0 0 24 24"
       >
-        <line
-          x1="0.42747"
-          y1="27.7406"
-          x2="54.4275"
-          y2="116.741"
-          stroke="black"
-        />
-        <line
-          x1="0.42747"
-          y1="0.740636"
-          x2="54.4275"
-          y2="89.7406"
-          stroke="black"
-        />
-        <line
-          y1="-0.5"
-          x2="104.101"
-          y2="-0.5"
-          transform="matrix(-0.518727 0.85494 0.85494 0.518727 108 1)"
-          stroke="black"
-        />
-        <line
-          y1="-0.5"
-          x2="104.101"
-          y2="-0.5"
-          transform="matrix(-0.518727 0.85494 0.85494 0.518727 108 28)"
-          stroke="black"
-        />
+        <g transform="rotate(180 12 12)">
+          <g transform="rotate(-90 12 12) translate(24 0) scale(-1 1)">
+            <g
+              fill="none"
+              stroke="#fffbe9"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            >
+              <path stroke-dasharray="20" stroke-dashoffset="20" d="M21 12H3.5">
+                <animate
+                  fill="freeze"
+                  attributeName="stroke-dashoffset"
+                  dur="0.3s"
+                  values="20;0"
+                />
+              </path>
+              <path
+                stroke-dasharray="12"
+                stroke-dashoffset="12"
+                d="M3 12L10 19M3 12L10 5"
+              >
+                <animate
+                  fill="freeze"
+                  attributeName="stroke-dashoffset"
+                  begin="0.3s"
+                  dur="0.2s"
+                  values="12;0"
+                />
+              </path>
+            </g>
+          </g>
+        </g>
       </svg>
     );
 }
