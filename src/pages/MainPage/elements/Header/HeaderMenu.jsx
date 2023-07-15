6165178467cba41a878fac484f6ef3a9f82acf28
OnigiriKing <oooNikita87@gmail.com>
2023-07-15 12:35:10 +0700

    Header update

--- src/pages/MainPage/elements/Header/HeaderMenu.jsx
+++ src/pages/MainPage/elements/Header/HeaderMenu.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import "./style.css";
+import { linkDinSvg, instSvg, gitHubSvg } from "../../../../svg/HeaderSvg";
 
 export default function HeaderMenu() {
 
     return (
-        <div className="header-menu">
-            <p>Home</p>
-            <p>About</p>
-            <p>Works</p>
+      <div className="header-menu">
+      <div>
+        <p>
+          Onigiri_King
+        </p>
+      </div>
+        <div className="nav-links">
+          <p>Home</p>
+          <p>About</p>
+          <p>Works</p>
         </div>
-    )
+        <div className="sm-links">
+          <a href="https://www.linkedin.com/in/onigiriking/" target="_blank">
+            {linkDinSvg}
+          </a>
+          <a href="https://github.com/OnigiriKing" target="_blank">
+            {gitHubSvg}
+          </a>
+          <a href="https://www.instagram.com/onigiri_king/" target="_blank">
+            {instSvg}
+          </a>
+        </div>
+      </div>
+    );
 }
