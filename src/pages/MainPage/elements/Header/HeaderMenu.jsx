fee4d231066fdfea6cc0c4c78ed80fd2f280ae8c
OnigiriKing <oooNikita87@gmail.com>
2023-07-15 19:24:42 +0700

    Demo svg color change (through state)

--- src/pages/MainPage/elements/Header/HeaderMenu.jsx
+++ src/pages/MainPage/elements/Header/HeaderMenu.jsx
@@ -1,30 +1,49 @@
 import React from "react";
 import "./style.css";
-import { linkDinSvg, instSvg, gitHubSvg } from "../../../../svg/HeaderSvg";
+import { linkDinSvg, instSvg, gitHubSvg, linkDinSvgActive, instSvgActive, gitHubSvgActive } from "../../../../svg/HeaderSvg";
 
 export default function HeaderMenu() {
 
+  const [isHover, setHover] = React.useState({ln: false, git: false, inst: false })
+
+  function changeHover(type, state) {
+    setHover({ ...isHover, [type]: state });
+  }
+
     return (
       <div className="header-menu">
-      <div>
-        <p>
-          Onigiri_King
-        </p>
-      </div>
+        <div>
+          <p>Onigiri_King</p>
+        </div>
         <div className="nav-links">
           <p>Home</p>
           <p>About</p>
           <p>Works</p>
         </div>
         <div className="sm-links">
-          <a href="https://www.linkedin.com/in/onigiriking/" target="_blank">
-            {linkDinSvg}
+          <a
+            href="https://www.linkedin.com/in/onigiriking/"
+            target="_blank"
+            onMouseEnter={() => changeHover("ln", true)}
+            onMouseLeave={() => changeHover("ln", false)}
+          >
+            {!isHover.ln ? linkDinSvg : linkDinSvgActive}
           </a>
-          <a href="https://github.com/OnigiriKing" target="_blank">
-            {gitHubSvg}
+          <a
+            href="https://github.com/OnigiriKing"
+            target="_blank"
+            onMouseEnter={() => changeHover("git", true)}
+            onMouseLeave={() => changeHover("git", false)}
+          >
+            {!isHover.git ? gitHubSvg : gitHubSvgActive}
           </a>
-          <a href="https://www.instagram.com/onigiri_king/" target="_blank">
-            {instSvg}
+          <a
+            href="https://www.instagram.com/onigiri_king/"
+            target="_blank"
+            onMouseEnter={() => changeHover("inst", true)}
+            onMouseLeave={() => changeHover("inst", false)}
+          >
+            {!isHover.inst ? instSvg : instSvgActive}
           </a>
         </div>
       </div>
