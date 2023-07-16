05d7e60f4e355e85b7341757c6716b32412bf196
OnigiriKing <oooNikita87@gmail.com>
2023-07-16 16:59:39 +0700

    images, imgFunction created

--- src/pages/MainPage/elements/MyWorks/Works.jsx
+++ src/pages/MainPage/elements/MyWorks/Works.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import "./style.css";
+import { mainCards } from "./img/imgFunction";
 
 
 export default function WorksScreen() {
 
     return (
-        <div className="works-screen">
-        </div>
-    )
+      <div className="works-screen">
+        <a className="image-cards">
+          <div className="big-card">
+            <img src={mainCards.budgetWebsite} />
+          </div>
+          <div className="big-card">
+            <img src={mainCards.cryptoWebsite} />
+          </div>
+          <div className="big-card">
+            <img src={mainCards.recipeWebsite} />
+          </div>
+          <div className="big-card">
+            {" "}
+            <img src={mainCards.portfolioWebsite} />
+          </div>
+
+          <div className="small-card"></div>
+          <div className="small-card"></div>
+          <div className="small-card"></div>
+          <div className="small-card"></div>
+        </a>
+      </div>
+    );
 }
---
+++ src/pages/MainPage/elements/MyWorks/img/imgFunction.js
@@ -0,0 +1,23 @@
+import budgetWebsite from "./budgetWebsite.png"
+import cryptoWebsite from "./cryptoWebsite.png"
+import recipeWebsite from "./recipeWebsite.png"
+import portfolioWebsite from "./portfolioWebsite.png"
+import reactIcon from "./reactIcon.png"
+import jsIcon from "./jsIcon.png"
+import htmlIcon from "./htmlIcon.png"
+import cssIcon from "./cssIcon.png"
+
+
+export const mainCards = {
+    budgetWebsite: [budgetWebsite],
+    cryptoWebsite: [cryptoWebsite],
+    recipeWebsite: [recipeWebsite],
+    portfolioWebsite: [portfolioWebsite]
+}
+
+export const smallCards = {
+  reactIcon: [reactIcon],
+  jsIcon: [jsIcon],
+  htmlIcon: [htmlIcon],
+  cssIcon: [cssIcon],
+};
