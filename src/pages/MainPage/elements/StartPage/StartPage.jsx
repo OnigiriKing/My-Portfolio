9191173bf9a425a86085b2a652760548bdbfc561
OnigiriKing <oooNikita87@gmail.com>
2023-07-18 14:21:37 +0700

    main page buttoms are added

--- src/pages/MainPage/elements/StartPage/StartPage.jsx
+++ src/pages/MainPage/elements/StartPage/StartPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import ArrorSvg from "../../../../svg/MainArror";
+import { Link } from "react-router-dom";
 
 export default function StartPage() {
   return (
@@ -8,7 +9,15 @@ export default function StartPage() {
       <div className="start-h1h2">
         <h1>ZAMYSHLIAEV NIKITA</h1>
       </div>
-      <ArrorSvg width={100}/>
+      <div>
+        <Link className="main-page-links" to={"/works"}>
+          My projects
+        </Link>
+        <Link className="main-page-links" to={"/contacts"}>
+          Hire me
+        </Link>
+      </div>
+      <ArrorSvg width={100} />
     </div>
   );
 }
